5e08deaecca9f1dd4de5f936bd14b2816b1c6525
JefersonT <jefersontorres.f@gmail.com>
2021-10-21 12:21:17 -0300

    Fist Commit

---
+++ src/com/dio/Main.java
@@ -0,0 +1,13 @@
+package com.dio;
+
+import com.dio.model.Order;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Order order = new Order("code1234");
+
+        System.out.println(order);
+    }
+}
